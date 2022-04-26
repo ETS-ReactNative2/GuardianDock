37f1a746f5d8ee200df9fae0735fab97f63aad68
0Nom4D <arthur.adam@epitech.eu>
2022-04-26 11:34:50 +0100

    GuardianDock : typescript-native-base - Fixing invisible icons with native-base/icons

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
     [
       'module-resolver',
       {
+        alias: {
+          "@native-base/icons": "@native-base/icons/lib",
+        },
         root: ['./src'],
         extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
       },
